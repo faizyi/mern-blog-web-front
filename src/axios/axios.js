270fc6934a8232c09d3dbd3cab121a34da20a293
faizyi <faizi13062000@gmail.com>
2025-03-27 11:27:23 +0300

    new dep

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosHandler = axios.create({
-    baseURL: "https://blog-backend-two-psi.vercel.app",
+    baseURL: "https://mern-blog-web-back-production.up.railway.app",
     headers: {"Content-Type" : "application/json"},
     withCredentials: true
 });
